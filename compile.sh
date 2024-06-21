5e0c5a1bf0a73b1772e2505645b4e2caebc5d3a6
Fil-Cyan-Tron <filtron11@gmail.com>
2024-06-21 12:35:50 +0200

    chore: Update compile.sh script to remove auxiliary files

--- compile.sh
+++ compile.sh
@@ -1,8 +1,19 @@
 pdflatex $1.tex
-if [ $2 == yesbib ]; then
+if [ $2 == "yesbib" ]; then
     biber $1
     pdflatex $1.tex
 else
     echo "Nessun file bibliografico da compilare"
 fi
 pdflatex $1.tex
+rm $1.aux
+rm $1.bcf
+rm $1.log
+rm $1.out
+rm $1.run.xml
+if [ $2 == "yesbib" ]; then
+    rm $1.bbl
+    rm $1.blg
+else
+    echo "nessun file bibliografico da rimuovere"
+fi
