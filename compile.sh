3ba9f6dd7defce2640be8e145f8f9da21ad25e2b
Fil-Cyan-Tron <filtron11@gmail.com>
2024-09-11 15:41:43 +0200

    Update MisuraIndotta.tex with improved section titles and corrected theorem on induced integration

--- compile.sh
+++ compile.sh
@@ -1,19 +1,12 @@
+if [ $2 == "yesbib" ]; then
+    biber $1
+fi
 pdflatex $1.tex
 if [ $2 == "yesbib" ]; then
     biber $1
     pdflatex $1.tex
+    biber $1
 else
     echo "Nessun file bibliografico da compilare"
 fi
 pdflatex $1.tex
-rm $1.aux
-rm $1.bcf
-rm $1.log
-rm $1.out
-rm $1.run.xml
-if [ $2 == "yesbib" ]; then
-    rm $1.bbl
-    rm $1.blg
-else
-    echo "nessun file bibliografico da rimuovere"
-fi
